7a742e3ee07ec2ff1593323460d48e12e649c649
Larry Hill <tomella1@gmail.com>
2017-10-23 22:21:58 +1100

    Make update incremental

--- source/postgres/pgToSolr.js
+++ source/postgres/pgToSolr.js
@@ -2,6 +2,9 @@ const config = require('./config');
 const featureFactory = config.solrFeatureFactory;
 const request = require('request');
 const solrAddEndpoint = "http://localhost:8983/solr/placenames/update?_=${now}&boost=1.0&commitWithin=1000&overwrite=true&wt=json";
+//const solrGetSupplyDate = "http://localhost:8983/solr/placenames/select?q=*:*&rows=1&sort=supplyDate+desc&wt=json";
+const solrGetSupplyDate = "http://placenames.geospeedster.com/select?q=*:*&rows=1&sort=supplyDate+desc&wt=json&fl=supplyDate";
+
 
 const mappings = config.properties;
 
@@ -24,64 +27,61 @@ const client = new Client({
    database: process.env.PLACENAMES_DB_DATABASE,
    password: process.env.PLACENAMES_DB_PASSWORD,
    port: process.env.PLACENAMES_DB_PORT,
- });
-
+});
 
 let pageSize = config.parameters.pageSize;
 
-client.connect((err, client, done) => {
-   let offset = 0;
-   // Handle connection errors
-   if (err) {
-      console.log(err);
-      return;
-   }
-
-   writeBlock(client, pageSize, offset).then(writeFinished);
-
-   function writeFinished(written) {
-      if (written) {
-         offset += pageSize;
-         writeBlock(client, pageSize, offset).then(writeFinished);
-      } else {
-         console.log("finished");
-         process.exit();
+getWhereClause().then(clause => {
+   client.connect((err, client, done) => {
+      let offset = 0;
+      // Handle connection errors
+      if (err) {
+         console.log(err);
+         return;
       }
-   }
 
-});
+      writeBlock(client, pageSize, offset).then(writeFinished);
 
+      function writeFinished(written) {
+         if (written) {
+            offset += pageSize;
+            writeBlock(client, pageSize, offset).then(writeFinished);
+         } else {
+            console.log("finished");
+            process.exit();
+         }
+      }
 
-function writeBlock(client, pageSize, offset) {
+      function writeBlock(client, pageSize, offset) {
+         return client.query('select * from "PLACENAMES" ' + clause + ' order by "ID" limit ' + pageSize + ' offset ' + offset + ';')
+            .then(res => {
+               let buffer = [];
 
-   return client.query('select * from "PLACENAMES" order by "ID" limit ' + pageSize + ' offset ' + offset + ';')
-      .then(res => {
-         let buffer = [];
+               rowsLength = res.rows.length;
 
-         rowsLength = res.rows.length;
+               res.rows.forEach(row => {
+                  let record = featureFactory();
+                  Object.keys(row).forEach(key => {
+                     let expected = mappings[key];
+                     let value = row[key];
 
-         res.rows.forEach(row => {
-            let record = featureFactory();
-            Object.keys(row).forEach(key => {
-               let expected = mappings[key];
-               let value = row[key];
-
-               if (typeof expected === "string") {
-                  record[expected] = value;
-               } else if (typeof expected === "object") {
-                  if (expected.type === "point") {
-                     record[expected.target][expected.index] = +value;
-                  }
-               }
+                     if (typeof expected === "string") {
+                        record[expected] = value;
+                     } else if (typeof expected === "object") {
+                        if (expected.type === "point") {
+                           record[expected.target][expected.index] = +value;
+                        }
+                     }
+                  });
+                  record.ll = record.location = record.location.join(' ');
+                  buffer.push(record);
+               });
+               addToSolr(buffer);
+               return rowsLength;
             });
-            record.ll = record.location = record.location.join(' ');
-            buffer.push(record);
-         });
-         addToSolr(buffer);
-         return rowsLength;
-      });
-}
-
+      }
+   });
+});
 
 let block = 0;
 function addToSolr(data) {
@@ -95,4 +95,33 @@ function addToSolr(data) {
       url: url
    }
    request.post(options);
+}
+
+function getWhereClause() {
+   return getLastSupplyDate().then(date => ' where "SUPPLY_DATE" > \'' + date + "'").catch(() => "");
+}
+
+function getLastSupplyDate() {
+   console.log("Getting last supply date");
+   return new Promise(function (resolve, reject) {
+      request.get(solrGetSupplyDate, function (error, response, body) {
+         let date = new Date(JSON.parse(body).response.docs[0].supplyDate);
+         // Minus one second
+         date.setSeconds(date.getSeconds() - 1);
+         resolve(pgFormatDate(date));
+      });
+   });
+}
+
+function pgFormatDate(date) {
+   return date.getFullYear() + "-" +
+      zeroPad(date.getMonth() + 1) + "-" +
+      zeroPad(date.getDate()) + " " +
+      zeroPad(date.getHours()) + ":" +
+      zeroPad(date.getMinutes()) + ":" +
+      zeroPad(date.getSeconds());
+}
+
+function zeroPad(d) {
+   return ("0" + d).slice(-2)
 }
