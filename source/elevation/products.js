e999b124826e6eb431d6919b505b3bfd8f80624b
Larry Hill <tomella1@gmail.com>
2017-11-17 00:58:08 +1100

    Add composite_id to products

--- source/elevation/products.js
+++ source/elevation/products.js
@@ -7,7 +7,8 @@ exports = module.exports = [
       file_name: "SRTM-derived 1 Second Digital Elevation Models Version 1.0",
       file_url: null,
       file_last_modified: new Date("1999-12-31"),
-      metadata_id: "aac46307-fce8-449d-e044-00144fdd4fa6"
+      metadata_id: "aac46307-fce8-449d-e044-00144fdd4fa6",
+      composite_id: "Geoscience Australia-aac46307-fce8-449d-e044-00144fdd4fa6"
    },
    {
       bbox: "ENVELOPE(113.0, 154, -10,-44)",
@@ -17,7 +18,8 @@ exports = module.exports = [
       file_name: "DEM SRTM 1Second Hydrologically Enforced",
       file_url: null,
       file_last_modified: new Date("1999-12-31"),
-      metadata_id: "a05f7893-0050-7506-e044-00144fdd4fa6"
+      metadata_id: "a05f7893-0050-7506-e044-00144fdd4fa6",
+      composite_id: "Geoscience Australia-a05f7893-0050-7506-e044-00144fdd4fa6"
    },
    {
       bbox: "ENVELOPE(113.760230, 162.000033, -9.205568,-45.949852)",
@@ -27,7 +29,8 @@ exports = module.exports = [
       file_name: "5 metre Digital Elevation Model (DEM)",
       file_url: null,
       file_last_modified: new Date("1999-12-31"),
-      metadata_id: "22be4b55-2465-4320-e053-10a3070a5236"
+      metadata_id: "22be4b55-2465-4320-e053-10a3070a5236",
+      composite_id: "Geoscience Australia-22be4b55-2465-4320-e053-10a3070a5236"
    },
    {
       bbox: "ENVELOPE(113.0, 154, -9,-44)",
@@ -37,6 +40,7 @@ exports = module.exports = [
       file_name: "25 metre Digital Elevation Model (DEM)",
       file_url: null,
       file_last_modified: new Date("1999-12-31"),
-      metadata_id: "22be4b55-2485-4320-e053-10a3070a5236"
+      metadata_id: "22be4b55-2485-4320-e053-10a3070a5236",
+      composite_id: "Geoscience Australia-22be4b55-2485-4320-e053-10a3070a5236"
    },
 ];
