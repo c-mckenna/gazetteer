4e1918ddcca64af1b713dc13abdbac0d64f31907
Larry Hill <tomella1@gmail.com>
2017-11-27 22:00:03 +1100

    Add some comments to metadataLoader

--- source/elevation/metadataLoader.js
+++ source/elevation/metadataLoader.js
@@ -2,15 +2,15 @@ var request = require("request");
 var solrHelper = require("./lib/solrhelper");
 var strat = require("./lib/strategies");
 var config = require("./metadataconfig").config;
-
 var loader = new strat.Strategies(config.baseUrl);
 
+// This is a factory method that takes the HTTP request and a template for updating
 var solrWriter = solrHelper.writer(request, "http://localhost:8983/solr/metadata/update?_=${now}&boost=1.0&commitWithin=1000&overwrite=true&wt=json");
 
 // Used to page through metadata_id
 var count = 0;
 
-
+// Phase one. Get the facets for metadata ID, that way we get all the unique ID's
 request({
    json: true,
    method: "GET",
@@ -24,6 +24,7 @@ request({
 });
 
 
+// Phase 2. Cycle through each of the metadata records and fetch other attributed like url, source etc
 function loadRecords(facets) {
    var containers = facets.map(facet => facet.name);
 
@@ -46,6 +47,7 @@ function loadRecords(facets) {
    });
 }
 
+// Phase 3: Get the full metadata from the source metadata repository.
 function loadRecord(record) {
    let writeRecord = {
       metadata_id: record.metadata_id,
@@ -54,22 +56,14 @@ function loadRecord(record) {
    };
 
    let strategy = loader.strategy(record.source);
-
    strategy.requestMetadata(record).then(metadata => {
       console.log(metadata)
       writeRecord.abstract = metadata.abstract;
       writeRecord.title = metadata.title;
       writeRecord.bbox = metadata.bbox;
       writeRecord.composite_id = writeRecord.source + "_" + writeRecord.metadata_id;
+      // Normally we send pages but at this stage there are so few we write one at a time. We will lilkely change this over time as
+      // the profile of the data changes.
       solrWriter([writeRecord]);
    });
 }
-
-/*
-
-qld.requestMetadata({
-   metadata_id: "3594F263-F8B4-4F21-923D-0E78B7D02C80"
-}).then(response => {
-   console.log(JSON.stringify(response, null, 3));
-});
-*/
